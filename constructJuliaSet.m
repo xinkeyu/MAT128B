322b419577963005aee3ad29030d71d23b5aa602
xinkeyu <xinkeyu1997@gmail.com>
2018-02-11 21:06:20 -0800

    part3 code finished (now with x+iy)

--- constructJuliaSet.m
+++ constructJuliaSet.m
@@ -1,31 +1,48 @@
 function [] = constructJuliaSet(x,y)
 %construct the Julia set for c = x+iy
 numofIt = 100; %number of iterations
-rangeUpper = 301; %determines the number of z_n's
+rangeUpper = 41; %determines the number of z_n's
 increment = 4/(rangeUpper-1);
 c = x+1i*y;
-phi1 = @(z) sqrt(z-c);
-phi2 = @(z) -sqrt(z-c);%positive and negative values of function to be chosen randomly
-figure
+%phi1 = @(z) sqrt(z-c);
+%phi2 = @(z) -sqrt(z-c);%positive and negative values of function to be chosen randomly
+figure %for plotting
 hold on
-for i = 1: rangeUpper
-    x_co = -2 + (rangeUpper-1)*increment;
+for i = 1: rangeUpper %choose initial values from -2 to 2
+    x_co = -2 + (rangeUpper-1)*increment;%real part
     for j = 1: rangeUpper 
-       y_co = -2 + (rangeUpper-1)*increment;
-       zn = x_co + y_co * 1i;
+       y_co = -2 + (rangeUpper-1)*increment;%imaginary part 
+       rnew = sqrt(sqrt((x_co-x)^2+(y_co-y)^2));
+       theta = atan((y_co-y)/(x_co-x));
+       if(x_co-x)<0
+           theta = theta + pi;
+       end
+       xnew = rnew * cos(theta/2);
+       ynew = rnew * sin(theta/2);
        k = 0;
-       znew = zn;
+
        while k <= numofIt
            k = k + 1;
-           randNum = round(rand()); %random number that determines which path to pursue
-           if (randNum == 1)
-              znew = phi1(znew);
-           else %randNum == 0
-              znew = phi2(znew);
+           randNum = round(rand()); %random number that determines which branch to pursue
+           if (randNum == 1)%positive branch
+              rnew = sqrt(sqrt((xnew-x)^2+(ynew-y)^2));
+              theta = atan((ynew-y)/(xnew-x));
+              if(xnew-x)<0
+                theta = theta + pi;
+              end
+              xnew = rnew * cos(theta/2);
+              ynew = rnew * sin(theta/2);
+           else %randNum == 0, negative branch
+              rnew = sqrt(sqrt((xnew-x)^2+(ynew-y)^2));
+              theta = atan((ynew-y)/(xnew-x));
+              if(xnew-x)<0
+                theta = theta + pi;
+              end
+              xnew = -rnew * cos(theta/2);
+              ynew = -rnew * sin(theta/2);
            end
        end
-       plot(real(znew), imag(znew),'.');
-       
+       plot(xnew, ynew, '.');
     end
     
 end
