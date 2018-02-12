d91580e4133ee004fcd8c1a5d17d354125a885b8
xinkeyu <xinkeyu1997@gmail.com>
2018-02-11 21:10:30 -0800

    added some comments

--- constructJuliaSet.m
+++ constructJuliaSet.m
@@ -4,14 +4,13 @@ numofIt = 100; %number of iterations
 rangeUpper = 41; %determines the number of z_n's
 increment = 4/(rangeUpper-1);
 c = x+1i*y;
-%phi1 = @(z) sqrt(z-c);
-%phi2 = @(z) -sqrt(z-c);%positive and negative values of function to be chosen randomly
+
 figure %for plotting
 hold on
 for i = 1: rangeUpper %choose initial values from -2 to 2
-    x_co = -2 + (rangeUpper-1)*increment;%real part
+    x_co = -2 + (rangeUpper-1)*increment; %real part
     for j = 1: rangeUpper 
-       y_co = -2 + (rangeUpper-1)*increment;%imaginary part 
+       y_co = -2 + (rangeUpper-1)*increment; %imaginary part 
        rnew = sqrt(sqrt((x_co-x)^2+(y_co-y)^2));
        theta = atan((y_co-y)/(x_co-x));
        if(x_co-x)<0
@@ -25,7 +24,7 @@ for i = 1: rangeUpper %choose initial values from -2 to 2
            k = k + 1;
            randNum = round(rand()); %random number that determines which branch to pursue
            if (randNum == 1)%positive branch
-              rnew = sqrt(sqrt((xnew-x)^2+(ynew-y)^2));
+              rnew = sqrt(sqrt((xnew-x)^2+(ynew-y)^2)); %values for the next iteration
               theta = atan((ynew-y)/(xnew-x));
               if(xnew-x)<0
                 theta = theta + pi;
@@ -33,7 +32,7 @@ for i = 1: rangeUpper %choose initial values from -2 to 2
               xnew = rnew * cos(theta/2);
               ynew = rnew * sin(theta/2);
            else %randNum == 0, negative branch
-              rnew = sqrt(sqrt((xnew-x)^2+(ynew-y)^2));
+              rnew = sqrt(sqrt((xnew-x)^2+(ynew-y)^2)); %essentially the same as above, just negate final value
               theta = atan((ynew-y)/(xnew-x));
               if(xnew-x)<0
                 theta = theta + pi;
