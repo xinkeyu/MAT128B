713988ee8fbdaf557b229aabe614ed5b696da063
xinkeyu <xinkeyu1997@gmail.com>
2018-02-18 21:23:06 -0800

    small change

--- orbit.m
+++ orbit.m
@@ -1,14 +1,15 @@
-function [vec, bounded] = orbit(x,y)
-%returns the orbit of 0 for phi(x) = x^2 + (x+iy)
+function [vec, connected] = orbit(x,y)
+%vec: the orbit of 0 for phi(x) = x^2 + (x+iy)
+%connected : 0 if not connected, 1 if connected
 z = x + 1i*y;
 count = 1;
-bounded = 0;
+connected = 0;
 while abs(z) < 100
    vec(count) = z;
    count = count+1;
    z = z^2 + x + 1i *y;
-   if (count >= 500)
-       bounded = 1;
+   if (count >= 500) %after 500 iterations, still not diverge
+       connected = 1;
        break;
    end
 end
